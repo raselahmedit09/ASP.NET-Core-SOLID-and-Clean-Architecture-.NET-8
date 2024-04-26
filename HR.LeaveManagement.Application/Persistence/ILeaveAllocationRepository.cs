eeed2b80deee2eb1afe7cdb3b15ea3effacdceb9
raselahmedit09 <rasel.ict09@gmail.com>
2024-04-26 11:29:47 +0600

    added application class

---
+++ HR.LeaveManagement.Application/Persistence/ILeaveAllocationRepository.cs
@@ -0,0 +1,8 @@
+﻿using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Persistence
+{
+    public interface ILeaveAllocationRepository : IGenericRepository<LeaveAllocation>
+    {
+    }
+}
