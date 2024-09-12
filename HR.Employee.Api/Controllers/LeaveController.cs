3cae488ebf7fa7f4737fa39ed511722900f86725
raselahmedit09 <rasel.ict09@gmail.com>
2024-09-12 15:15:32 +0600

    api to api call using ocelot api gateway. (rest client api call that generated by nswag)

---
+++ HR.Employee.Api/Controllers/LeaveController.cs
@@ -0,0 +1,26 @@
+﻿using APIRestClient.HR.LeaveManagementModule;
+using APIRestClient.HR.LeaveManagementModule.Contacts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.Employee.Api.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LeaveController : ControllerBase
+    {
+        private LeaveManagementModuleAPI _leaveManagementModuleAPI;
+
+        public LeaveController(LeaveManagementModuleAPI leaveManagementModuleAPI)
+        {
+            _leaveManagementModuleAPI = leaveManagementModuleAPI;
+        }
+
+
+        [HttpGet]
+        public async Task<List<LeaveTypeDto>> apiGetWay_LeaveTypes_GetAsync()
+        {
+            return await _leaveManagementModuleAPI.LeaveTypes_GetAsync();
+        }
+    }
+}
---
+++ RestApiClient/ServiceRegistrationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using APIRestClient.HR.LeaveManagementModule;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RestApiClient
+{
+    public static class ServiceRegistrationExtensions
+    {
+        public static IServiceCollection RestApiClientGetewayServices(this IServiceCollection services)
+        {
+            services.AddHttpClient<LeaveManagementModuleAPI>((serviceProvider, client) =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7090"); // Set the base URL for the HttpClient
+            });
+
+            services.AddScoped<LeaveManagementModuleAPI>(serviceProvider =>
+            {
+                var httpClient = serviceProvider.GetRequiredService<HttpClient>();
+                var baseUrl = "https://localhost:7090"; // Provide the base URL
+                return new LeaveManagementModuleAPI(baseUrl, httpClient);
+            });
+
+            return services;
+        }
+    }
+}
